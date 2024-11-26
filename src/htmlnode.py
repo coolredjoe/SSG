4d84853daa4d51988cf8796a9be404a5c6a58ece
coolredjoe <jeroenriepma@outlook.com>
2024-11-26 10:40:42 +0100

    htmlnode updates

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,6 +1,16 @@
 class HTMLNode():
-    def __init__(self, tag, value, children, props):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        result = ""
+        for i in self.props:
+            result += " "
+            result += f"{i}=\"{self.props[i]}\""
+        return result[1:]

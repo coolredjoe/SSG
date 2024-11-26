2b896e42a10eecf1ce797744701099c71e4aa20c
coolredjoe <jeroenriepma@outlook.com>
2024-11-26 16:01:49 +0100

    parent node class added

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,5 +30,21 @@ class LeafNode(HTMLNode):
         if self.tag == None:
             return self.value
         return f"<{self.tag + self.props_to_html()}>{self.value}</{self.tag}>"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children=None, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        result = ""
+        if self.tag == None:
+            raise ValueError("Tag can not be None")
+        if self.children = None:
+            raise ValueError("Children can not be None")
+        for i in self.children:
+            result += i.to_html()
+        return f"<{self.tag}>" + result + f"</{self.tag}>"
+        
 
         
